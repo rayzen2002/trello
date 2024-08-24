0338d9f989ced5a1d1767bd1bcc9535fe6649754
rayzen2002 <emanuelzsmj@gmail.com>
2024-08-23 23:49:44 -0300

    Adding CORS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
 import { editCard } from './routes/edit-card.js'
+import fastifyCors from '@fastify/cors'
 export const app = fastify({
   connectionTimeout: 1000 * 60 * 2, // 2 minutos
 })
 
+app.register(fastifyCors, {
+  origin: true,
+})
 app.register(editCard)
 app.get('/', () => {
   return 'hello world'
